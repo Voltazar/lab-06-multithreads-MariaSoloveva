a50d7c4ac830621d87138492ad974706d7be63e1
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-12-02 16:46:17 +0300

    Update multithreads.cpp

--- multithreads.cpp
+++ multithreads.cpp
@@ -9,13 +9,24 @@
 
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/sinks/sink.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
 
 
 
-//  namespace logging = boost::log;
 
 void Logging()
 {
+    // Construct the sink
+    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> text_sink;
+    boost::shared_ptr< text_sink> sink = boost::make_shared< text_sink >();
+
+    // Add a stream to write log to
+    sink->locked_backend()->add_stream(
+            boost::make_shared< std::ofstream >("sample.log"));
+
+    // Register the sink in the logging core
+    boost::log::core::get()->add_sink(sink);
+
     boost::log::core::get()->set_filter
     (
         boost::log::trivial::severity >= boost::log::trivial::trace
@@ -48,14 +59,9 @@ int main(int args, char* argv[])
     std::srand(time(NULL));
     int M = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
-    args = 2;
     if (args == 2)
     {
         M = atoi(argv[1]);
-    }
-    else
-    {
-        throw std::runtime_error("Error");
     }
 
     for (int m = 0; m < M; ++m)
