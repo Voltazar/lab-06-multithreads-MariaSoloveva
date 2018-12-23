5170c56fe39bc3a54f5aac5970f26562c7934922
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-12-23 14:30:45 +0300

    Update multithreads.cpp

--- multithreads.cpp
+++ multithreads.cpp
@@ -18,7 +18,8 @@
 void Logging()
 {
     // Construct the sink
-    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> text_sink;
+    typedef boost::log::sinks::synchronous_sink
+            <boost::log::sinks::text_ostream_backend> text_sink;
     boost::shared_ptr< text_sink> sink = boost::make_shared< text_sink >();
 
     // Add a stream to write log to
@@ -29,27 +30,22 @@ void Logging()
     boost::log::core::get()->add_sink(sink);
 
     boost::log::core::get()->set_filter
-    (
-        boost::log::trivial::severity >= boost::log::trivial::trace
-        ||  boost::log::trivial::severity >= boost::log::trivial::info
-    );
+            (boost::log::trivial::severity >= boost::log::trivial::trace
+                    ||  boost::log::trivial::severity >= boost::log::trivial::info);
 }
 
 
 void Find(int m)
 {
-    Logging();
-    while(true)
+    for (auto i = 0; i < 65535; ++i)
     {
-        const std::vector<unsigned char> data_{(unsigned char)std::rand(), (unsigned char)std::rand(),
-                                               (unsigned char)std::rand(), (unsigned char)std::rand()};
+        const std::vector<unsigned char> data_{
+            (unsigned char)std::rand(), (unsigned char)std::rand(),
+            (unsigned char)std::rand(), (unsigned char)std::rand()};
         const std::string hash = picosha2::hash256_hex_string(data_);
-        if (hash.find(("0000"), hash.size() - 5) != std::string::npos)
-        {
+        if (hash.find(("0000"), hash.size() - 5) != std::string::npos) {
             BOOST_LOG_TRIVIAL(trace) << "Hash with '0000'" << hash;
-        }
-        else
-        {
+        } else {
             BOOST_LOG_TRIVIAL(info) << "Thread : " << m << " hash : " << hash;
         }
     }
@@ -64,7 +60,7 @@ int main(int args, char* argv[])
     {
         M = atoi(argv[1]);
     }
-
+    Logging();
     for (int m = 0; m < M; ++m)
     {
         threads.emplace_back(Find, m);
